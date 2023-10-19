ec0a63630f57f4b2b9b44941f362ef25d119a1d1
Zbiku <Zbiku@MSI>
2023-10-19 22:46:20 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        double a, b, c, delta;
+        Console.WriteLine("Podaj a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj c: ");
+        c = double.Parse(Console.ReadLine());
+        delta = Math.Pow(b,2) - 4 * a * c;
+        Console.WriteLine("Delta wynosi: " + delta);
+        Console.ReadKey();
+    }
+}
